89afaae3e72f932ddc6d0ef0c2b7c73158457da5
Neelesh Poojari <npoojari@sapient.com>
2018-08-05 20:05:31 +0530

    React-ES6-Project_using_Firebase

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDV3E3-eMhE6jtbQgf8cARz4Q1nK6Y6JNo",
+    authDomain: "catch-of-the-day-neeleshkp.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-neeleshkp.firebaseio.com"  
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+export default base; 
+ 
---
+++ src/components/StorePicker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { format } from 'url';
+import { getFunName } from '../helpers.js';
+
+
+class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    
+    
+    myInput = React.createRef();
+
+    // goToStore (event) {
+    //     event.preventDefault();
+    //     console.log(this);
+    // }
+    goToStore = event => {
+        event.preventDefault();
+       const storeName = this.myInput.value.value;
+       this.props.history.push(`/store/${storeName}`);
+    }
+
+    render() {
+        return <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store </h2>
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store --></button>
+        </form>
+
+    }
+}
+
+export default StorePicker;
